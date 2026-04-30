dd555facba566ed268552f38f86cfce432b3b544
João Pedro Floriano <joaopedrofloriano21@gmail.com>
2026-04-30 20:19:36 -0300

    Link do usuário

---
+++ src/models/UsuarioModel.js
@@ -0,0 +1,14 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/db");
+
+const Usuario = sequelize.define("Usuarios", {
+  idUsuarios: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  nome: { type: DataTypes.STRING(255), allowNull: false },
+  email: { type: DataTypes.STRING(255), allowNull: false },
+  senha_hash: { type: DataTypes.STRING(255), allowNull: false },
+  telefone: { type: DataTypes.STRING(45), allowNull: false },
+}, { timestamps: false });
+
+module.exports = Usuario;
+
+
