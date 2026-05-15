ddd8ebac9d19b225974c70452cc6baea3a19131b
João Pedro Floriano <joaopedrofloriano21@gmail.com>
2026-05-15 18:48:20 -0300

    correções finais

--- src/models/LinkCardapioModel.js
+++ src/models/LinkCardapioModel.js
@@ -1,9 +1,13 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../config/db");
 
-const LinkCardapio = sequelize.define("Link_Cardapio", {
-  idLink: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  link: { type: DataTypes.STRING(255) },
-}, { timestamps: false });
+const LinkCardapio = sequelize.define(
+  "Link_Cardapio",
+  {
+    idLink: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    link: { type: DataTypes.STRING(255) },
+  },
+  { timestamps: false, tableName: "Link_Cardapio", freezeTableName: true }
+);
 
 module.exports = LinkCardapio;
